ef871b4869e1d09ff1a7dfb666340218a9c799d7
EisukeKikuchiFromIwate <73880312+EisukeKikuchiFromIwate@users.noreply.github.com>
2021-01-06 16:28:43 +0900

    JavaScript記述

--- app/javascript/shindan.js
+++ app/javascript/shindan.js
@@ -1,7 +1,9 @@
 $(function () {
   $(".btn").on("click", function () {
+    
     $(this).closest("div").css("display", "none");
     id = $(this).attr("href");
-    $(id).addClass("fit").show("slow");
+    $(id).addClass("fit").fadeIn("slow").show();
   });
+  
 });
